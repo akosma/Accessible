498715c78c66aaa16f4819d833c433c48cc66c4b
Adrian Kosmaczewski <akosma@me.com>
2016-09-12 14:57:05 +0200

    Added error management to the form

--- Accessible/FormViewController.swift
+++ Accessible/FormViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FormViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
@@ -24,8 +25,11 @@ class FormViewController: UIViewController {
     @IBOutlet weak var cityField: UITextField!
     @IBOutlet weak var countryField: UITextField!
     @IBOutlet weak var favoriteSwitch: UISwitch!
+    @IBOutlet var labels: [UILabel]!
+    @IBOutlet var fields: [UITextField]!
     
     var customer: Customer?
+    let errorPrefix = "* "
     
     override func viewDidLoad() {
         if let customer = customer {
@@ -36,14 +40,12 @@ class FormViewController: UIViewController {
             cityField.text = customer.city
             countryField.text = customer.country
             ageField.text = "\(customer.age)"
-            favoriteSwitch.setOn(customer.favorite, animated: false)
+            favoriteSwitch.isOn = customer.favorite
         }
     }
     
     @IBAction func save(_ sender: AnyObject) {
-        if !validate() {
-            nameLabel.textColor = UIColor.red
-        }
+        validate()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -53,10 +55,43 @@ class FormViewController: UIViewController {
         }
     }
     
-    func validate() -> Bool {
-        if nameField.text?.lengthOfBytes(using: .utf8) == 0 {
-            return false
+    func validate() {
+        var counter = 0
+        var errors = 0
+        for field in fields {
+            let label = labels[counter]
+            if field.text?.lengthOfBytes(using: .utf8) == 0 {
+                label.textColor = .red
+                
+//                let font = UIFont.preferredFont(forTextStyle: .body)
+//                label.font = UIFont.boldSystemFont(ofSize: font.pointSize)
+//                
+//                if let text = label.text,
+//                    !text.hasPrefix(errorPrefix) {
+//                    label.text = "\(errorPrefix)\(text)"
+//                }
+//                errors += 1
+            }
+            else {
+                label.textColor = .black
+
+//                let font = UIFont.preferredFont(forTextStyle: .body)
+//                label.font = UIFont.systemFont(ofSize: font.pointSize)
+//
+//                if let text = label.text,
+//                    text.hasPrefix(errorPrefix) {
+//                    let index = text.index(text.startIndex, offsetBy: 2)
+//                    label.text = text.substring(from: index)
+//                }
+            }
+            counter += 1
         }
-        return true
+        errorLabel.isHidden = errors == 0
+    }
+}
+
+extension FormViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        validate()
     }
 }
